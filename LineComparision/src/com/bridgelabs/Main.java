c5ea8be5d2225c27a272fb3dbc470968d87409c4
csumn <csumn13@gmail.com>
2022-03-13 21:38:08 +0530

    both UC2 and UC3 added

--- LineComparision/src/com/bridgelabs/Main.java
+++ LineComparision/src/com/bridgelabs/Main.java
@@ -6,10 +6,10 @@ public class Main {
 		LineComparision line1 = new LineComparision(77,64,77,72);
 		double lengthOfline_1 = line1.calculate_length();
 		System.out.println("Length of Line one is : " +lengthOfline_1);
-
 		LineComparision line2 = new LineComparision(77,72,82,72);
 		double lengthOfline_2 = line2.calculate_length();
 		System.out.println("Length of Line two is : " +lengthOfline_2);
+		LineComparision.check_Equality(lengthOfline_1, lengthOfline_2);
 		LineComparision.compare_Lines(lengthOfline_1, lengthOfline_2);
 	}
 }
