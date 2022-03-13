8095cd6e6af96f92a3564ec35af6c36a18a61545
csumn <csumn13@gmail.com>
2022-03-13 21:20:42 +0530

    length cal added

---
+++ LineComparision/src/com/bridgelabs/LineComparision.java
@@ -0,0 +1,19 @@
+package com.bridgelabs;
+
+public class LineComparision {
+	private double x1;
+	private double y1;
+	private double x2;
+	private double y2;
+
+	public LineComparision(double X1, double Y1, double X2, double Y2) {
+		this.x1 = X1;
+		this.y1 = Y1;
+		this.x2 = X2;
+		this.y2 = Y2;
+	}
+	public double calculate_length() {
+		double LengthOfLine = Math.sqrt( ( x2 - x1 ) * ( x2 - x1 ) + ( y2 - y1 ) * ( y2 - y1 ));
+		return LengthOfLine;	
+	}
+}
---
+++ LineComparision/src/com/bridgelabs/Main.java
@@ -0,0 +1,15 @@
+package com.bridgelabs;
+
+public class Main {
+
+	public static void main(String[] args) {
+		LineComparision line1 = new LineComparision(77,72,82,72);
+		double lengthOfline_1 = line1.calculate_length();
+		System.out.println("Length of Line one is : " +lengthOfline_1);
+
+		LineComparision line2 = new LineComparision(77,64,77,72);
+		double lengthOfline_2 = line2.calculate_length();
+		System.out.println("Length of Line two is : " +lengthOfline_2);
+	}
+
+}
