008f0ed3575fb0ad03146825e68821d7ee2e2ce8
csumn <csumn13@gmail.com>
2022-03-13 21:30:46 +0530

    Comparing two lines

--- LineComparision/src/com/bridgelabs/Main.java
+++ LineComparision/src/com/bridgelabs/Main.java
@@ -3,13 +3,13 @@ package com.bridgelabs;
 public class Main {
 
 	public static void main(String[] args) {
-		LineComparision line1 = new LineComparision(77,72,82,72);
+		LineComparision line1 = new LineComparision(77,64,77,72);
 		double lengthOfline_1 = line1.calculate_length();
 		System.out.println("Length of Line one is : " +lengthOfline_1);
 
-		LineComparision line2 = new LineComparision(77,64,77,72);
+		LineComparision line2 = new LineComparision(77,72,82,72);
 		double lengthOfline_2 = line2.calculate_length();
 		System.out.println("Length of Line two is : " +lengthOfline_2);
-		LineComparision.check_Equality(lengthOfline_1, lengthOfline_2);
+		LineComparision.compare_Lines(lengthOfline_1, lengthOfline_2);
 	}
 }
